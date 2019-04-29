f5b49c16012ad1508eeeb08fba61c117c8499cc6
ks563 <karen.shea90@gmail.com>
2019-04-28 19:49:03 -0500

    added sql schema, route structure

---
+++ attendanceRoutes/apiRoutes.js
@@ -0,0 +1,16 @@
+const db = require("../models");
+const path = require("path");
+
+module.exports = function (app) {
+    
+//admin route to pull all attendance
+    app.get("/api/attendance/admin", function (req, res) {
+        db.conDO_db.findAll({where:{}}).then(function(attendance) {
+            res.send(attendance);
+            //need buttons on html side to sort data
+        })
+    })
+
+    //
+}
+
