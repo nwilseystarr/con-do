ffe3f3527bb9f6d5b093c83aa29e25d1072188b4
Brendan K <brendanmkelly94@gmail.com>
2019-04-30 00:55:48 -0500

    configured postgress user and pw

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const app = express();
+let app = express();
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -12,9 +12,9 @@ const db = require("./db/models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-require("./attendanceRoutes/apiRoutes.js")(app);
-require("./attendanceRoutes/htmlRoutes.js")(app);
+// console.log(require("atstendanceRoutes/apiRoutes"));
+require("./attendanceRoutes/apiRoutes")(app);
+// require("./attendanceRoutes/htmlRoutes")(app);
 
 // Send every request to the React app
 // Define any API routes before this runs
