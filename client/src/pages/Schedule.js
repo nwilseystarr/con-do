da16c29df0eb792de18ab97e7f954d16f3e32c8a
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2019-05-03 18:14:55 -0500

    [client/src/app.js| db | schedule.js] added new page for schedule.js, deleted old schemas, updated app.js with schedule component

---
+++ client/src/pages/Schedule.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+import { Link } from "react-router-dom";
+import API from "../utils/API"
+
+class Schedule extends Component {
+
+}
+
+export default Schedule;
--- server/db/schema/committee_staff_schema.sql
+++
@@ -1,9 +0,0 @@
-USE con_doDB;
-
-CREATE TABLE committee_staff (
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(50) NOT NULL, 
-    email VARCHAR(50) NOT NULL,
-    committee VARCHAR(50) NOT NULL, 
-	primary key (id)
-);
