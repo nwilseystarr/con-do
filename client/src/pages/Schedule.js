6f5210c71537c09f806b34cb1e518da28169a47e
LAMorris <morris.leaha@gmail.com>
2019-05-04 13:37:22 -0500

    deleted pages/Schedule.js

--- client/src/pages/Schedule.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-import { Link } from "react-router-dom";
-import API from "../utils/API"
-
-class Schedule extends Component {
-
-}
-
-export default Schedule;
