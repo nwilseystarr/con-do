8703a0bba811ebc126d984f82bdd0d59c6e5dbcc
ks563 <karen.shea90@gmail.com>
2019-05-04 21:11:01 -0500

    attendance route, attendance front-end

---
+++ client/src/components/Event/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Navbar from "../Navbar";
+
+class Attendance extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                {/* attendance table */}
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
+
--- server/routes/api/attendance.js
+++ server/routes/api/attendance.js
@@ -1,50 +1,28 @@
-// const db = require("../../db/models");
-// const router = require("express").Router();
+const db = require("../../db/models");
+const router = require("express").Router();
+const isAuthenticated = require("../../db/config/middleware/isAuthenticated");
 
 
-// //admin route to pull all attendance
-// router.route("/admin")
-//     .get(function (req, res) {
-//         db.events.findAll({}).then(function(attendance) {
-//             res.send(attendance);
-//             //need buttons on html side to sort data
-//         })
-//     })
+router.route("/")
+    .get(function (req, res) {
+        db.events.findAll({
+            
+        }).then(function(attendance) {
+            res.send(attendance);
+            //need buttons on html side to sort data
+        })
+    })
 
-//     //faculty
-// router.route("/faculty")
-//     .get(function (req, res) {
-//         db.attendance.findAll({
-//             where: {
-//             //faculty school matches student school
-//             }
-//         }).then(function (attendance) {
-//             res.send(attendance)
-//         })
-//     })
-
-// router.route("/staff")
+// router.route("")
 //     .get(function (req, res) {
 //         db.events.findAll({
 //             where: {
-//             //staff committee === delegate committee
+//                 user: req.params.user
 //             }
-//         }).then(function (attendance) {
+//         }).then(attendance => {
 //             res.send(attendance)
 //         })
 //     })
 
-// router.route("/delegate")
-//     .put(function (req, res) {
-//         db.eventss.update(
-//             { attendance: req.params.attend
-//             }, {
-//                 where: req.params.id
-//             }
-//         ).then(function (rowsUpdated) {
-//             res.json(rowsUpdated)
-//         })
-//     })
 
-// module.exports = router;
-
+module.exports = router;
