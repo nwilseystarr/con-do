dd2ae24f135e94cf3a444ab6c0f2dad71708c4b3
ks563 <karen.shea90@gmail.com>
2019-05-09 21:06:50 -0500

    favicon change

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,14 +2,12 @@ const router = require("express").Router();
 const userRoutes = require("./users");
 const commmitteeRoutes = require("./committees");
 const schoolRoutes = require("./schools");
-const attendanceRoutes = require("./attendance");
 const eventRoutes = require("./events");
 
 // api routes go here
 router.use("/users", userRoutes);
 router.use("/committees", commmitteeRoutes);
 router.use("/schools", schoolRoutes);
-router.use("/attendance", attendanceRoutes);
 router.use("/events", eventRoutes);
 
 module.exports = router;
