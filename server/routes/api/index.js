5e1558af82d0c697c804eb4b980f9ce19f3896c8
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2019-05-17 18:28:14 -0500

    [server/routes/API/contactForm.js | server/routes/API/index.js] final push for the working contact form

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -5,6 +5,7 @@ const schoolRoutes = require("./schools");
 const eventRoutes = require("./events");
 const measureRoutes = require("./measures");
 const chatRoutes = require("./chat");
+const contactForm = require("./contactForm");
 
 // api routes go here
 router.use("/users", userRoutes);
@@ -13,5 +14,6 @@ router.use("/schools", schoolRoutes);
 router.use("/events", eventRoutes);
 router.use("/measures", measureRoutes);
 router.use("/chat", chatRoutes);
+router.use("/contactForm", contactForm);
 
 module.exports = router;
